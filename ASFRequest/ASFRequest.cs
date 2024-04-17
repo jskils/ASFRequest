a59e210dca0c3dbecf6c0ec46073b4b812f5799c
yangjingsong <yangjingsong@leenzee.com>
2024-04-17 11:13:09 +0800

    feat:完成POST返回html,json

---
+++ ASFRequest/ASFRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using ArchiSteamFarm.Core;
+using ArchiSteamFarm.Plugins.Interfaces;
+using JetBrains.Annotations;
+
+namespace ASFRequest;
+
+#pragma warning disable CA1812 // ASF uses this class during runtime
+[UsedImplicitly]
+internal sealed class ASFRequest : IGitHubPluginUpdates {
+	public string Name => nameof(ASFRequest);
+	public string RepositoryName => "jskils/ASFRequest";
+	public Version Version => typeof(ASFRequest).Assembly.GetName().Version ?? throw new InvalidOperationException(nameof(Version));
+
+	public Task OnLoaded() {
+		ASF.ArchiLogger.LogGenericInfo($"Hello {Name}!");
+
+		return Task.CompletedTask;
+	}
+}
+#pragma warning restore CA1812 // ASF uses this class during runtime
